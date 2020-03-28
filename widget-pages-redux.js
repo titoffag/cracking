c68cfd4b750c3d537a5cc8cbf0c869a4a710eb24
Aleksander Titov <aleksandr_titov@epam.com>
2020-03-29 01:00:45 +0400

    typo correction in file name and formatting of tabs

--- widget-pages-redux.js
+++ widget-pages-redux.js
@@ -1,69 +1,73 @@
 const applicationGlobalState = {
-    pages : {
-        byId : {
-            "page1" : {
-                id : "page1",
-                title: "page title 1",
-                widgetList: ["widget1", "widget2"]
-            },
-            "page2" : {
-                id : "page2",
-                titlePage: "page title 2",
-                widgetList: ["widget2", "widget3"]
-            }
-        },
-        allIds : ["page1", "page2"]
+  pages: {
+    byId: {
+      page1: {
+        id: "page1",
+        title: "page title 1",
+        widgetList: ["widget1", "widget2"]
+      },
+      page2: {
+        id: "page2",
+        titlePage: "page title 2",
+        widgetList: ["widget2", "widget3"]
+      }
     },
-    widgets : {
-        byId : {
-            "widget1" : {
-                id : "widget1",
-                title: "widget title 1",
-                body: "lorem"
-            },
-            "widget2" : {
-                id : "widget2",
-                title: "widget title 2",
-                body: "lorem"
-            },
-            "widget3" : {
-                id : "widget3",
-                title: "widget title 3",
-                body: "lorem"
-            },
-        },
-        allIds : ["widget1", "widget2", "widget3"]
-    }
+    allIds: ["page1", "page2"]
+  },
+  widgets: {
+    byId: {
+      widget1: {
+        id: "widget1",
+        title: "widget title 1",
+        body: "lorem"
+      },
+      widget2: {
+        id: "widget2",
+        title: "widget title 2",
+        body: "lorem"
+      },
+      widget3: {
+        id: "widget3",
+        title: "widget title 3",
+        body: "lorem"
+      }
+    },
+    allIds: ["widget1", "widget2", "widget3"]
+  }
 };
 
-const ADD_WIDGET_TO_PAGE = 'ADD_WIDGET_TO_PAGE';
-const EDIT_TEXT_OF_WIDGET = 'EDIT_TEXT_OF_WIDGET';
+const ADD_WIDGET_TO_PAGE = "ADD_WIDGET_TO_PAGE";
+const EDIT_TEXT_OF_WIDGET = "EDIT_TEXT_OF_WIDGET";
 function addWidgetToPage(payload) {
-    return {
-        type: ADD_WIDGET_TO_PAGE,
-        payload
-    };
+  return {
+    type: ADD_WIDGET_TO_PAGE,
+    payload
+  };
 }
 function editTextOfWidget(payload) {
-    return {
-        type: EDIT_TEXT_OF_WIDGET,
-        payload
-    };
+  return {
+    type: EDIT_TEXT_OF_WIDGET,
+    payload
+  };
 }
 
 const store = {
-    dispatch: (action) => {
-        console.log(rootApplicationReducer(undefined, action))
-    }
+  dispatch: action => {
+    console.log(rootApplicationReducer(undefined, action));
+  }
 };
-store.dispatch(addWidgetToPage({
+store.dispatch(
+  addWidgetToPage({
     widgetId: "widget101",
     pageId: "page1"
-}));
-store.dispatch(editTextOfWidget({
+  })
+);
+store.dispatch(
+  editTextOfWidget({
     widgetId: "widget1",
-    widgetBody: "bla-bla"
-}));
+    widgetBody: "blah-blah"
+  })
+);
 
 function rootApplicationReducer(state = applicationGlobalState, action) {
   switch (action.type) {
@@ -71,31 +75,34 @@ function rootApplicationReducer(state = applicationGlobalState, action) {
       return {
         ...state,
         pages: {
-            byId: {
-                ...state.pages.byId,
-                [action.payload.pageId]: {
-                    ...state.pages.byId[action.payload.pageId],
-                    widgetList: [...state.pages.byId[action.payload.pageId].widgetList, action.payload.widgetId]
-                },
-            },
-            allIds: [...state.pages.allIds, action.payload.widgetId],
-        },
+          byId: {
+            ...state.pages.byId,
+            [action.payload.pageId]: {
+              ...state.pages.byId[action.payload.pageId],
+              widgetList: [
+                ...state.pages.byId[action.payload.pageId].widgetList,
+                action.payload.widgetId
+              ]
+            }
+          },
+          allIds: [...state.pages.allIds, action.payload.widgetId]
+        }
       };
     case EDIT_TEXT_OF_WIDGET:
       return {
         ...state,
         widgets: {
-            byId: {
-                ...state.widgets.byId,
-                [action.payload.widgetId]: {
-                    ...state.widgets.byId[action.payload.widgetId],
-                    body: action.payload.widgetBody
-                },
-            },
-            allIds: [...state.widgets.allIds, action.payload.widgetId],
-        },
+          byId: {
+            ...state.widgets.byId,
+            [action.payload.widgetId]: {
+              ...state.widgets.byId[action.payload.widgetId],
+              body: action.payload.widgetBody
+            }
+          },
+          allIds: [...state.widgets.allIds, action.payload.widgetId]
+        }
       };
     default:
-      return state
-  } 
+      return state;
+  }
 }
