ba1355b6791392eb150c6f89ffcce5610e5bbb33
Aleksander Titov <aleksandr_titov@epam.com>
2020-06-22 18:29:08 +0400

    add tests to curry function

--- curry.js
+++ curry.js
@@ -3,9 +3,24 @@ function curry(fn) {
     if (args.length >= fn.length) {
       return fn.apply(this, args);
     } else {
-      return function(...restArgs) {
+      return function (...restArgs) {
         return curried.apply(this, args.concat(restArgs));
       };
     }
   };
 }
+
+function abc(a, b, c) {
+  return a + b + c;
+}
+
+function abcdef(a, b, c, d, e, f) {
+  return a + b + c + d + e + f;
+}
+
+console.log(curry(abc)("A")("B")("C")); // 'ABC'
+console.log(curry(abc)("A", "B")("C")); // 'ABC'
+console.log(curry(abc)("A", "B", "C")); // 'ABC'
+
+console.log(curry(abcdef)("A")("B")("C")("D")("E")("F")); // 'ABCDEF'
+console.log(curry(abcdef)("A", "B", "C")("D", "E", "F")); // 'ABCDEF'
