03c86373ceb19644ed76f3c64c5b7653ea825eab
Aleksandr Titov <44509969+titoffag@users.noreply.github.com>
2021-02-02 15:51:39 +0400

    Update bind function

--- bind.js
+++ bind.js
@@ -1,6 +1,13 @@
-function bind(fn, context) {
-  const args = [].slice.call(arguments, 2);
-  return function() {
-    return fn.apply(context, args);
-  };
+// Реализовать метод bind
+function bind(func, context, ...bindArgs){
+  return function(...fnArgs){
+    return func.apply(context, bindArgs.concat(fnArgs));
+  }
 }
+
+let foo = function(...args){
+	console.log(this, ...args);
+}
+
+let bar = bind(foo, {a: 1});
+console.log(bar([1, 2, 3])); //{a: 1}, [1, 2, 3]
